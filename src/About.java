c3acd5d7cf4bab060e8864f2fdf97d53283da179
Daniel J Kirsch <djkirsch326@gmail.com>
2018-12-27 12:56:36 -0500

    Removing code I added for curiosity

--- src/About.java
+++ src/About.java
@@ -16,11 +16,9 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
-import java.util.List;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import net.dv8tion.jda.core.Permission;
-import net.dv8tion.jda.core.entities.Guild;
 
 public class About extends Command{
 	public About() {
@@ -32,10 +30,6 @@ public class About extends Command{
 		String link = event.getJDA().asBot().getInviteUrl(recommendedPerms);
 		String message = "Hello! I am HaGaon HaMachane! I am a discord bot that can quote from an assortment of Jewish Holy Texts!\n [Invite](" + link + ") me to your server!\n[Find me on Github!](https://github.com/djkirsch/HaGaonHaMachane)";
 		SendVerse.sendEmbed("About", message, event);
-		List<Guild> guilds = event.getJDA().getGuilds();
-		for(Guild a : guilds)
-			System.out.println(a.getName());
-
 	}
 
 }
